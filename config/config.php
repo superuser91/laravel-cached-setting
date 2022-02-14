93051c8462743df4725443f02a88e527b8f7e933
dungnt2 <dungnt2.ctv@gmail.com>
2022-02-14 15:09:27 +0700

    fix check policy

--- config/config.php
+++ config/config.php
@@ -4,11 +4,6 @@ return [
     'settings' => [
         'prefix' => '/admin/settings/',
         'middleware' => ['auth:admin'],
-        'permissions' => [
-            'index' => 'settings.index',
-            'create' => 'settings.create',
-            'update' => 'settings.update',
-            'delete' => 'settings.delete'
-        ]
+        'policy' => 'App\\Policies\\SettingPolicy'
     ]
 ];
--- src/CachedSettingServiceProvider.php
+++ src/CachedSettingServiceProvider.php
@@ -3,11 +3,24 @@
 namespace Vgplay\LaravelCachedSetting;
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Vgplay\LaravelCachedSetting\Actions\ListSetting;
+use Vgplay\LaravelCachedSetting\Setting\Setting;
 
 class CachedSettingServiceProvider extends ServiceProvider
 {
+    /**
+     * Get the policies defined on the provider.
+     *
+     * @return array
+     */
+    public function policies()
+    {
+        return [
+            Setting::class => config('vgplay.settings.policy'),
+        ];
+    }
+
     public function boot()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'vgplay');
